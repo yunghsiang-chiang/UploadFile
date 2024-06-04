309418f60dd635e770751c8820f75c653ef0b00c
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2024-06-04 16:59:58 +0800

    2024/06/04 16:59:00

---
+++ UploadFile/Controllers/FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace UploadFile.Controllers
+{
+    [EnableCors(origins: "http://10.10.3.75:8080", headers: "*", methods: "*")]
+    public class FileUploadController : ApiController
+    {
+        [HttpPost]
+        public string[] UploadFiles()
+        {
+            HttpFileCollection files = HttpContext.Current.Request.Files;
+            string[] path = new string[files.Count];
+            for(var i = 0; i < files.Count; i++)
+            {
+                HttpPostedFile file = files[i];
+                string roothPath = "~/Upload/" + file.FileName;
+                path[i] = roothPath.Substring(1);
+                file.SaveAs(HttpContext.Current.Server.MapPath(roothPath));
+            }
+
+            return path;
+        }
+    }
+}

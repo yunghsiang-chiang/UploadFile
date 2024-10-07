fde860b20592c3d0bcea90318833eff5267a49c8
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2024-10-07 17:12:18 +0800

    2024/10/7 17:12

--- UploadFile/Controllers/FileUploadController.cs
+++ UploadFile/Controllers/FileUploadController.cs
@@ -13,19 +13,45 @@ namespace UploadFile.Controllers
     public class FileUploadController : ApiController
     {
         [HttpPost]
-        public string[] UploadFiles()
+        public IHttpActionResult UploadFiles()
         {
-            HttpFileCollection files = HttpContext.Current.Request.Files;
-            string[] path = new string[files.Count];
-            for(var i = 0; i < files.Count; i++)
+            try
             {
-                HttpPostedFile file = files[i];
-                string roothPath = "~/Upload/" + file.FileName;
-                path[i] = roothPath.Substring(1);
-                file.SaveAs(HttpContext.Current.Server.MapPath(roothPath));
-            }
+                HttpFileCollection files = HttpContext.Current.Request.Files;
+                if (files.Count == 0)
+                {
+                    return Json(new { uploaded = false, error = new { message = "No file uploaded." } });
+                }
 
-            return path;
+                string[] paths = new string[files.Count];
+                for (var i = 0; i < files.Count; i++)
+                {
+                    HttpPostedFile file = files[i];
+                    string rootPath = "~/Upload/" + file.FileName;
+                    string fullPath = HttpContext.Current.Server.MapPath(rootPath);
+                    file.SaveAs(fullPath);
+
+                    // 構建圖片的 URL
+                    string fileUrl = "http://internal.hochi.org.tw:8081" + rootPath.Substring(1);
+                    paths[i] = fileUrl;
+                }
+
+                // 判斷是否為 CKEditor 請求
+                if (HttpContext.Current.Request.Headers["User-Agent"] != null &&
+                    HttpContext.Current.Request.Headers["User-Agent"].Contains("CKEditor"))
+                {
+                    // CKEditor 的返回格式
+                    return Json(new { uploaded = true, url = paths[0] });
+                }
+
+                // 默認返回現有應用的字串數組格式
+                return Ok(paths);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { uploaded = false, error = new { message = ex.Message } });
+            }
         }
     }
+
 }
